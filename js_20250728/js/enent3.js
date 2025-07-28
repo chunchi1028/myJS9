07978bb7303db07be6a6f37e34594cc5ab934ef9
chunchi1028 <wsjennt1028@gmail.com>
2025-07-28 16:25:28 +0800

    20250728

---
+++ js_20250728/js/enent3.js
@@ -0,0 +1,18 @@
+let nav = document.querySelector('nav');
+let navLink = document.querySelectorAll('nav ul li a');
+
+// 頁面捲動時
+window.addEventListener('scroll', () => {
+    // console.log(window.pageYOffset);
+    if (window.pageYOffset != 0) {
+        nav.style.backgroundColor = "rgba(0,0,0,0.7)";
+        navLink.forEach(a => {
+            a.style.color = "#fff";
+        });
+    } else {
+        nav.style.backgroundColor = "#ccc";
+        navLink.forEach(a => {
+            a.style.color = "#333";
+        });
+    }
+})
